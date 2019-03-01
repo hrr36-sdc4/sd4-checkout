888378bd0b570e97b8bb190c5105416dd6aeede0
rdkerr <rogerdkerr@gmail.com>
2019-03-01 15:38:50 -0700

    Formatting

--- server/index.js
+++ server/index.js
@@ -15,45 +15,47 @@ app.route('/rooms/:listingId')
   .get((req, res) => {
     db.getRoom(req.params.listingId)
       .then(records => res.send(records))
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   })
   .delete((req, res) => {
     db.deleteListing(req.params.listingId)
       .then(() => res.end())
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   })
   .update((req, res) => {
     db.updateListing(req.params.listingId, req.body)
       .then(() => res.end())
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   })
 
 app.post('/rooms/', (req, res) => {
   db.addRoom(req.body)
     .then(() => res.end())
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 });
 
 app.route('/rooms/bookings/:listingId')
   .get((req, res) => {
     db.getBookings(req.params.listingId)
-      .then(records => res.send(records));
+      .then(records => res.send(records))
+      .catch(err => console.log(err));
   })
   post((req, res) => {
     db.bookRoom(req.params.listingId, req.body)
-      .then(() => res.end());
+      .then(() => res.end())
+      .catch(err => console.log(err));
   });
 
 app.route('/rooms/bookings/:bookingId')
   .delete((req, res) => {
     db.deleteBooking(req.params.bookingId)
       .then(() => res.end())
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   })
   .update((req, res) => {
     db.updateBooking(req.params.bookingId, req.body)
       .then(() => res.end())
-      .catch(err => console.log(err))
+      .catch(err => console.log(err));
   });
 
 var server = app.listen(port, function() {
