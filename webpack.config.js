6db9f094a70115bfa62bc4291a3d6c72d12fb219
rdkerr <rogerdkerr@gmail.com>
2019-03-11 14:29:51 -0700

    Added laoders for css and scss

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,31 @@ const common = {
           presets: ['react', 'es2015', 'env']
         },
       },
+      {
+        test : /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test : /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
     ],
   }, 
   plugins: [
