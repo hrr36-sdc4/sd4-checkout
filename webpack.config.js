cb69a1d569e097a15736c5314f0c2c1de7f3b851
rdkerr <rogerdkerr@gmail.com>
2019-03-12 11:21:56 -0700

    Remove comments from bundles

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ const common = {
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,   // enable source maps to map errors (stack traces) to modules
       output: {
-        comments: false, // remove all comments
+        comments: true, // remove all comments
       },
     }),
   ]
